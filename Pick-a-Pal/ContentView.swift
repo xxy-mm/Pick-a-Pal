ff77aa658c95255fa501ca9992949c0492f379de
darian <xxy.moming@gmail.com>
2024-04-17 21:20:16 +0800

    Initial Commit

---
+++ Pick-a-Pal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pick-a-Pal
+//
+//  Created by Darian Mitchell  on 2024/4/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Darian Mitchell  on 2024/4/17.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
