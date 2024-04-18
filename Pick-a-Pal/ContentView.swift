0ed4ca04cc9f4e9e0e2533eb7ca8360ec7ea1418
darian <xxy.moming@gmail.com>
2024-04-18 10:45:06 +0800

    complete

--- Pick-a-Pal/ContentView.swift
+++ Pick-a-Pal/ContentView.swift
@@ -8,12 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var names: [String] = []
+    @State private var nameToAdd = ""
+    @State private var pickedName = ""
+    @State private var shouldRemovePickedName = false
+
     var body: some View {
+
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+            VStack(spacing: 8) {
+                Image(systemName: "person.3.sequence.fill")
+                    .foregroundStyle(.tint)
+                    .symbolRenderingMode(.hierarchical)
+                Text("Pick-a-Pal")
+            }
+            .font(.title)
+            .bold()
+            
+            Text(pickedName.isEmpty ? " " : pickedName)
+                .font(.title2)
+                .bold()
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            List {
+                ForEach(names, id: \.self) { name in
+                    Text(name)
+                }
+            }
+//            .clipShape(RoundedRectangle(cornerRadius: 8))
+            .cornerRadius(8)
+
+            TextField("Add name", text: $nameToAdd)
+                .autocorrectionDisabled()
+                .onSubmit {
+                    let name = nameToAdd.trimmingCharacters(in: .whitespacesAndNewlines)
+                    
+                    if !name.isEmpty, !names.contains(name) {
+                        names.append(name)
+                    }
+                   
+                    nameToAdd = ""
+                }
+            Toggle("Remove picked names", isOn: $shouldRemovePickedName)
+            
+            Button {
+                if let randomName = names.randomElement() {
+                    pickedName = randomName
+                    
+                    if shouldRemovePickedName {
+                        names.removeAll { name in
+                            name == randomName
+                        }
+                    }
+                } else {
+                    pickedName = ""
+                }
+            } label: {
+                Text("Pick Random Name")
+                    .padding(.vertical, 8)
+                    .padding(.horizontal, 16)
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
         }
         .padding()
     }
